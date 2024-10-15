6eab7ac14a96e4d1ed7ec7221be2ef4ccdcdd963
Samuele <samueletasciotti@icloud.com>
2024-10-15 12:25:46 +0200

    feat: root colors

---
+++ src/components/navbar.vue
@@ -0,0 +1,31 @@
+<script>
+export default {
+    
+}
+</script>
+
+<template>
+
+    <!-- Container -->
+    <div class="container-fluid text-center d-flex">
+        
+        <!-- Logo -->
+        <div>
+            <!-- logo -->
+            <img src="src/public/img/logo_bnb" alt=""> 
+            <!-- titolo -->
+            <span></span>
+        </div>
+
+        <!-- Searcbar -->
+        <div></div>
+
+        <!-- Login -->
+        <div></div>
+    </div>
+
+</template>
+
+<style scoped>
+
+</style>
