ec4839dc5ae8ef4fcae226b9ba38a121ed21c8ba
Samuele <samueletasciotti@icloud.com>
2024-10-18 16:36:59 +0200

    fix: include bootstrap JS

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import { createApp } from 'vue'
 import './style.scss'
 import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap'
 import App from './App.vue'
 
 createApp(App).mount('#app')
