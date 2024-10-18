03bbeb2d361e9db96e186cf7cb37b0643ae2a288
Samuele <samueletasciotti@icloud.com>
2024-10-18 22:40:47 +0200

    fix: refactoring

--- src/components/navbar.vue
+++ src/components/navbar.vue
@@ -14,10 +14,12 @@ export default {
 <template>
   <!-- Container Up -->
   <div
-    class="container-lg text-center d-flex justify-content-between align-items-center  mt-3 mb-5"
+    class="container-lg text-center d-flex justify-content-between align-items-center mt-3 mb-5"
   >
     <!-- Logo -->
-    <div class="logo d-none d-md-block align-items-center justify-content-around">
+    <div
+      class="logo d-none d-md-block align-items-center justify-content-around"
+    >
       <div>
         <!-- logo -->
         <img src="../../public/img/logo_bnb.png" alt="" />
@@ -43,64 +45,59 @@ export default {
           <i class="fa-solid fa-user"></i>
         </button>
         <ul class="dropdown-menu">
-          <li><a class="dropdown-item" href="http://127.0.0.1:8000/register">Register</a></li>
-          <li><a class="dropdown-item" href="http://127.0.0.1:8000/login">Login</a></li>
+          <li>
+            <a class="dropdown-item" href="http://127.0.0.1:8000/register"
+              >Register</a
+            >
+          </li>
+          <li>
+            <a class="dropdown-item" href="http://127.0.0.1:8000/login"
+              >Login</a
+            >
+          </li>
         </ul>
       </div>
 
       <!-- globe -->
-
     </div>
   </div>
 </template>
 
 <style lang="scss" scoped>
-
+// Use of Scss
+@use "../style.scss" as *;
 a {
   text-decoration: none;
-  color: #28a745;
+  color: $green-color;
 }
 
-a > i {
-  font-size: 28px;
-}
-a:hover {
-  color: green;
-}
-
-.type{
-  flex-grow: 1;
-}
-
-/* Gestione delle voci di Type */
-.radius {
-  border-radius: 25px;
+.type {
+  // Barra di ricerca
+  flex-grow: 1; // Fa si che la barra di ricerca si adatti in proposione al monitor;
 }
 
 .logo {
   box-sizing: border-box;
   display: flex;
-}
+  div {
+    display: flex;
+    align-items: center;
+  }
 
-.logo div {
-  display: flex;
-  align-items: center;
-}
+  img {
+    width: 80px;
+  }
 
-/* Dimensioni del Logo */
-.logo img {
-  width: 80px;
-}
-
-.logo span {
+  span {
   font-size: 40px;
   font-weight: lighter;
   padding-bottom: 5px;
 }
+}
+
 
 .btn {
   width: 3.5rem;
   border-radius: 2em;
 }
-
 </style>
