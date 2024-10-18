eb06a6823e2fc0f0a644744f2f81afdc95285702
Samuele <samueletasciotti@icloud.com>
2024-10-18 22:51:24 +0200

    fix: Refactoring css of Searchbar

--- src/components/searchbar.vue
+++ src/components/searchbar.vue
@@ -1,73 +1,76 @@
 <script>
 export default {
-    name: 'Searchbar',
-}
+  name: "Searchbar",
+};
 </script>
 
 <template>
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+  <link
+    rel="stylesheet"
+    href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css"
+  />
   <div class="search-bar">
     <div class="search-item">
-        <input type="text" id="destination" placeholder="Cerca destinazioni">
-        <div>
-            <button class="search-btn"><i class="fa-solid fa-magnifying-glass"></i></button>
-        </div>
+      <input type="text" id="destination" placeholder="Cerca destinazioni" />
+      <div>
+        <button class="search-btn">
+          <i class="fa-solid fa-magnifying-glass"></i>
+        </button>
+      </div>
     </div>
-</div>
-
-
+  </div>
 </template>
 
-
 <style lang="scss" scoped>
-@use '../style.scss' as *;
+@use "../style.scss" as *;
 
 * {
-    box-sizing: border-box;
+  box-sizing: border-box;
 }
 
 .search-bar {
-    display: flex;
-    align-items: center;
-    border: 1px solid --white-color;
-    border-radius: 50px;
-    width: 90%;
-    height: 3.5rem;
-    margin: 0 auto;
-    background-color: white;
-    box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
-    font-weight: 6px;
+  display: flex;
+  align-items: center;
+  border: 1px solid $white-color;
+  border-radius: 50px;
+  width: 90%;
+  height: 3.5rem;
+  margin: 0 auto;
+  background-color: $white-color;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+  font-weight: 6px;
 }
 
 .search-item {
-    width: 100%;
-    height: 100%;
-    position: relative;
-    padding: 10px 15px;
-    display: flex;
-    align-items: center;
-}
+  width: 100%;
+  height: 100%;
+  position: relative;
+  padding: 10px 15px;
+  display: flex;
+  align-items: center;
 
-.search-item:hover, .search-item-last:hover {
+  &:hover,
+  .search-item-last:hover {
     background-color: gainsboro;
     box-shadow: 0 2px 5px rgba(0, 0, 0, 0.1);
     border-radius: 50px;
-}
+  }
 
-.search-item:hover input, .search-item-last:hover input {
+  &:hover input,
+  .search-item-last:hover input {
     background-color: gainsboro;
-}
+  }
 
-.search-item input {
+  input {
     border: none;
     outline: none;
     padding: 0 5px;
     font-size: 14px;
     width: 100%;
     padding-right: 50px;
-}
+  }
 
-.search-btn {
+  .search-btn {
     position: absolute;
     background-color: $green-color;
     border: none;
@@ -75,13 +78,13 @@ export default {
     padding: 10px 15px;
     cursor: pointer;
     font-size: 16px;
-    color: white;
+    color: $white-color;
     right: 5px;
     top: calc(50% - 22px);
-}
+  }
 
-.search-btn:hover {
+  .search-btn:hover {
     background-color: #2c9444;
+  }
 }
-
 </style>
