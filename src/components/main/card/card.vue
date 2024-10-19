e6244645fd6bddb118d64901ad28670ed4d4f127
Samuele <samueletasciotti@icloud.com>
2024-10-19 12:12:48 +0200

    fix: img card

--- src/components/main/card/card.vue
+++ src/components/main/card/card.vue
@@ -2,7 +2,7 @@
 // import rating from '../components/main/card/rating.vue';
 
 export default {
-  name: 'Card',
+  name: "Card",
   components: {
     // rating
   },
@@ -10,41 +10,48 @@ export default {
     return {
       imageIndex: 0,
       images: [
-        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSgrA66Je1VOoed7Lcw95T3vIRSJceEuwTsxw&s',
-        'https://a0.muscache.com/im/pictures/81dca5d6-5a86-49bc-8eca-4a8610a07d27.jpg',
-        'https://www.toptrade.it/files/2023/05/airbnb-3399753_640.jpg'
+        "https://www.lombardia.info/wp-content/uploads/sites/112/lago-como-villa.jpg",
+        "https://www.lombardia.info/wp-content/uploads/sites/112/menaggio-lago-como.jpg",
+        "https://www.scopriremilano.com/f/italia/milan/guia/lago-como-m.jpg",
       ],
       rating: 0,
     };
   },
   methods: {
     nextImage() {
-        this.imageIndex = (this.imageIndex + 1) % this.images.length;
-        
+      this.imageIndex = (this.imageIndex + 1) % this.images.length;
     },
 
     prevImage() {
-        this.imageIndex = (this.imageIndex - 1 + this.images.length) % this.images.length;
-    }
+      this.imageIndex =
+        (this.imageIndex - 1 + this.images.length) % this.images.length;
+    },
   },
-}
+};
 </script>
 
 <template>
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+  <link
+    rel="stylesheet"
+    href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css"
+  />
   <div class="card-container">
     <div class="carousel">
-      <button @click="prevImage" class="arrow prev"><i class="fa-solid fa-arrow-left"></i></button>
+      <button @click="prevImage" class="arrow prev">
+        <i class="fa-solid fa-arrow-left"></i>
+      </button>
       <div class="carousel-images">
         <img
-          v-for="(image, index) in images" 
+          v-for="(image, index) in images"
           :key="index"
           :src="image"
           class="carousel-image"
           v-show="index === imageIndex"
         />
       </div>
-      <button @click="nextImage" class="arrow next"><i class="fa-solid fa-arrow-right"></i></button>
+      <button @click="nextImage" class="arrow next">
+        <i class="fa-solid fa-arrow-right"></i>
+      </button>
     </div>
     <div class="card-info">
       <h3></h3>
@@ -56,7 +63,6 @@ export default {
 </template>
 
 <style lang="scss">
-
 .card-container {
   width: 300px;
   border: 1px solid white;
@@ -72,6 +78,11 @@ export default {
 .carousel-images {
   display: flex;
   transition: transform 0.5s ease-in-out;
+  img{
+    width: 300px;
+    height: 300px;
+    object-fit: contain;
+  }
 }
 
 .arrow {
@@ -86,37 +97,33 @@ export default {
   padding: 2.5px 10px;
   z-index: 1;
   border-radius: 50%;
-}
-
-.arrow:hover {
+  :hover {
     background-color: rgba(144, 143, 143, 0.9);
-   }
+  }
 
-.arrow.prev {
-  left: 10px;
+  .card-info {
+    padding: 16px;
+    background-color: white;
+  }
+  .card-info h3 {
+    margin: 0;
+    font-size: 18px;
+  }
+  .card-info p {
+    margin: 4px 0;
+    font-size: 14px;
+  }
+  .rating {
+    font-size: 16px;
+    color: black;
+  }
 }
 
 .arrow.next {
-  right: 10px;
-}
+    right: 10px;
+  }
 
-.card-info {
-  padding: 16px;
-  background-color: white;
-}
-
-.card-info h3 {
-  margin: 0;
-  font-size: 18px;
-}
-
-.card-info p {
-  margin: 4px 0;
-  font-size: 14px;
-}
-
-.rating {
-  font-size: 16px;
-  color: black
+.arrow .arrow.prev {
+  left: 10px;
 }
 </style>
