f1f937574517c9d34aa3a4aa5fbbd8109cee7d20
Samuele <samueletasciotti@icloud.com>
2024-10-23 15:48:32 +0200

    feat: 404 page error

---
+++ src/components/NotFound.vue
@@ -0,0 +1,18 @@
+<script>
+export default {
+    name: 'NotFound'
+};
+</script>
+
+<template>
+<div class="container text-center">
+
+    <!-- Titol -->
+    <h1>404</h1>
+
+    <!-- Content -->
+    <h3>Pagina non trovata</h3>
+</div>
+</template>
+
+<style></style>
