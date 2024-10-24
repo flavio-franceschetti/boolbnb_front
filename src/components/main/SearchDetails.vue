eba39fe403c1fa1b5b9c7ebe465f2cf6d78579c7
flavio-franceschetti <flaviofranceschetti98@gmail.com>
2024-10-24 21:00:47 +0200

    feat: filter functionality

--- src/components/main/SearchDetails.vue
+++ src/components/main/SearchDetails.vue
@@ -2,40 +2,68 @@
 import { RouterLink, RouterView } from "vue-router";
 import Card from "./card/Card.vue";
 import { store } from "../../store";
+import Filters from "../Filters.vue";
 import axios from "axios";
 
 export default {
   name: "SearchDetails",
   components: {
     Card,
+    Filters,
   },
   data() {
     return {
       apartments: [], // Lista degli appartamenti
-      // Questo è l'indirizzo che arriva dalla pagina home dopo la ricerca
-      address: this.$route.query.address || '', 
-      distance: this.$route.query.distance || 20, // Distanza predefinita
-      rooms: this.$route.query.rooms || 1, // Numero minimo di stanze predefinito
-      beds: this.$route.query.beds || 1, // Numero minimo di letti predefinito
-      services: this.$route.query.services ? this.$route.query.services.split(',') : [] // Lista dei servizi selezionati
+      address: this.$route.query.address || "",
+      distance: Number(this.$route.query.distance) || 20, // Converti in Number
+      rooms: Number(this.$route.query.rooms) || 1, // Converti in Number
+      beds: Number(this.$route.query.beds) || 1,
+      services: this.$route.query.services
+        ? this.$route.query.services.split(",")
+        : [],
+      availableServices: [], // Servizi disponibili
     };
   },
   watch: {
     "$route.query": {
       handler() {
         // Aggiorna le proprietà locali in base ai nuovi parametri di query
-        this.address = this.$route.query.address || '';
+        this.address = this.$route.query.address || "";
         this.distance = this.$route.query.distance || 20;
         this.rooms = this.$route.query.rooms || 1;
         this.beds = this.$route.query.beds || 1;
-        this.services = this.$route.query.services ? this.$route.query.services.split(',') : [];
+        this.services = this.$route.query.services
+          ? this.$route.query.services.split(",")
+          : [];
 
         // Chiama l'API con i parametri aggiornati
         this.getApi();
       },
-    }
+    },
   },
   methods: {
+    getAllServices() {
+      axios.get(store.apiUrl + "services").then((response) => {
+        this.availableServices = response.data.services;
+      });
+    },
+
+    applyFilters(newFilters) {
+      // Rimuovi i servizi se non ce ne sono
+      const services = newFilters.services.length
+        ? newFilters.services.join(",")
+        : "";
+      this.$router.push({
+        query: {
+          ...this.$route.query,
+          distance: Number(newFilters.distance), // Converti in Number
+          rooms: Number(newFilters.rooms), // Converti in Number
+          beds: Number(newFilters.beds),
+          services: services,
+        },
+      });
+    },
+
     getApi() {
       // Chiamata API per ottenere i dati degli appartamenti
       axios
@@ -45,21 +73,30 @@ export default {
             distance: this.distance,
             rooms: this.rooms,
             beds: this.beds,
-            services: this.services.join(','), // Converte l'array di servizi in una stringa per la chiamata API
-          }
+            services: this.services.join(","),
+          },
         })
         .then((response) => {
-          this.apartments = response.data.apartments; // Salva gli appartamenti ottenuti nella variabile
+          this.apartments = response.data.apartments;
+          // Salva gli appartamenti ottenuti nella variabile
         })
         .catch((error) => {
           console.error("Errore durante la richiesta API:", error);
           // Considera di aggiungere un feedback per l'utente qui in caso di errore
         });
     },
+    updateQuery(param, value) {
+      // Aggiorna i parametri di query nella rotta
+      this.$router.push({ query: { ...this.$route.query, [param]: value } });
+    },
+    searchApartments() {
+      this.getApi(); // Chiama l'API per cercare gli appartamenti
+    },
   },
   mounted() {
     // Chiama l'API con l'indirizzo inserito nella ricerca per ottenere gli appartamenti
     this.getApi();
+    this.getAllServices();
   },
 };
 </script>
@@ -68,7 +105,17 @@ export default {
   <!-- Titolo della pagina esposizione -->
   <h1>Ricerca avanzata</h1>
 
-  <!-- Container gestito tramite bootstrap grill per il responsive -->
+  <!-- Filtri -->
+  <Filters
+    :distance="distance"
+    :rooms="rooms"
+    :beds="beds"
+    :services="services"
+    :servicesList="availableServices"
+    @update-filters="applyFilters"
+  />
+
+  <!-- Container per gli appartamenti -->
   <div class="container">
     <div class="row">
       <div
@@ -86,10 +133,26 @@ export default {
 @use "../../style.scss" as *;
 
 h1 {
-  // Titolo della pagina principale
   text-align: center;
   font-size: 40px;
   font-weight: lighter;
   padding-bottom: 5px;
+}
+
+.search-bar {
+  display: flex;
+  justify-content: center;
+  margin-bottom: 20px;
+}
+
+.filters {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-bottom: 20px;
+}
+
+.filters label {
+  margin: 5px 0;
 }
 </style>
--- src/components/searchbar.vue
+++ src/components/searchbar.vue
@@ -1,3 +1,5 @@
+<!-- Searchbar.vue -->
+
 <script>
 import axios from "axios";
 import { store } from "../store";
@@ -7,50 +9,28 @@ export default {
   name: "Searchbar",
   data() {
     return {
-      servicesList: [],
-      address: '',       // Address input
-      distance: 20,      // Default distance
-      rooms: 1,          // Default rooms
-      beds: 1,           // Default beds
-      services: [],
-    };;
+      address: "", // Address input
+    };
   },
   methods: {
-    getServices(){
-      axios
-      .get(store.apiUrl + "services")
-      .then((response) => {
-        this.servicesList = response.data.services;
-      })
-      .catch((error) => {
-        console.error("Errore durante la richiesta API:", error);
-      });
-    },
     searchAddress() {
       this.$router.push({
         name: "searchDetails",
         query: {
           address: this.address,
-          distance: this.distance,
-          rooms: this.rooms,
-          beds: this.beds,
-          services: this.services.join(','),
-        }
+        },
       });
     },
-    
+
     getSearchBox() {
       axios.get(store.apiUrl + "tomtomKey").then((response) => {
-        // SEZIONE DELLA SEARCHBOX
-        // recuper la chiave dell'api inserita in config
         const apiKey = response.data.apiKey;
-        // queste sono le impostazioni della searchBox di tomtom
         let options = {
           searchOptions: {
-            key: apiKey, // l' API key
-            language: "it-IT", // linguaggio della ricerca
-            limit: 10, // numero di risultati visualizzati nel dropdown dei risultati
-            countrySet: ["IT"], // Limita la ricerca all'Italia
+            key: apiKey,
+            language: "it-IT",
+            limit: 10,
+            countrySet: ["IT"],
           },
           autocompleteOptions: {
             key: apiKey,
@@ -58,44 +38,29 @@ export default {
             countrySet: ["IT"],
           },
           labels: {
-            suggestions: {
-              brand: "Brand Suggerito",
-              category: "Categoria Suggerita",
-            },
-            placeholder: "Inserisci l'indirizzo", // placeholder della barra di ricerca
-            noResultsMessage: "Nessun risultato trovato", // messaggio quando si inserisce un valore che non viene trovato
+            placeholder: "Inserisci l'indirizzo",
+            noResultsMessage: "Nessun risultato trovato",
           },
         };
 
-        // Creo la serachbox con il costruttore di tomtom tt.plugins.SearchBox
-        // tt.services è il servizio di ricerca che tomtom usa per gestire le richieste
-        // option è la variabile creata con tutte le opzioni dentro
         let ttSearchBox = new tt.plugins.SearchBox(tt.services, options);
-
-        // Con getSearchBoxHTML mi restituisce il blocco html dove è inserita la searchbox già creata
         let searchBoxHTML = ttSearchBox.getSearchBoxHTML();
-        // inserisco la searchbox nel mio div html tramite l append
         document.getElementById("search-box-container").append(searchBoxHTML);
 
-        // Seleziono l'input della search box e gli assegno un 'name'
         let searchInput = document.querySelector("#search-box-container input");
-        searchInput.setAttribute("autocomplete", "off"); // aggiungo l'attributo id
+        searchInput.setAttribute("autocomplete", "off");
 
-        // aggiungiamo l'evento al tasto enter per la ricerca degli appartamenti
         searchInput.addEventListener("keyup", (event) => {
-          // event.key restituisce il tasto che viene cliccato, quindi se uguale ad enter esegui il codice
           if (event.key === "Enter") {
-            // assegno il valore dell'input search ad address
             this.address = searchInput.value;
-            this.searchAddress(this.address);
+            this.searchAddress();
           }
         });
-        // recupero la lente della barra di ricerca
+
         let svgLens = document.querySelector(
           ".tt-search-box-input-container div svg"
         );
-        // aggiungo l'evento al click che fa partire la ricerca
-        svgLens.addEventListener("click", (event) => {
+        svgLens.addEventListener("click", () => {
           this.address = searchInput.value;
           this.searchAddress();
         });
@@ -104,40 +69,11 @@ export default {
   },
   mounted() {
     this.getSearchBox();
-    this.getServices()
   },
 };
 </script>
-
 <template>
   <div id="search-box-container"></div>
-  <div class=" d-flex mt-2 justify-content-between">
-    <div class="d-flex flex-column">
-      <label for="distance">Distanza (km):</label>
-      <input v-model="distance" id="distance" type="number" placeholder="Distanza massima" />
-    </div>
-    <div class="d-flex flex-column">
-      <label for="rooms">Numero minimo di stanze:</label>
-      <input v-model="rooms" id="rooms" type="number" placeholder="Numero di stanze" />
-    </div>
-    <div class="d-flex flex-column">
-      <label for="beds">Numero minimo di letti:</label>
-      <input v-model="beds" id="beds" type="number" placeholder="Numero di letti" />
-    </div>
-  </div>
-  <div class="d-flex justify-content-between">
-        <div v-for="service in servicesList" :key="service.id">
-          <label>
-            <input 
-              type="checkbox" 
-              :value="service.name" 
-              v-model="services"  
-            />
-            {{ service.name }}
-          </label>
-        </div>
-      </div>
-  
 </template>
 
 <style lang="scss">
