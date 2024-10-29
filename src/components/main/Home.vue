1df4c92779c8b489e61c181d61392908afbc4b06
flavio-franceschetti <flaviofranceschetti98@gmail.com>
2024-10-29 11:18:52 +0100

    fix: add some padding

--- src/components/main/Home.vue
+++ src/components/main/Home.vue
@@ -38,7 +38,7 @@ export default {
   <!-- Titolo della pagina esposizione -->
   <h1 class="my-5">I nostri appartamenti in evidenza</h1>
   <!-- Container gestito tramite bootstrap grill per il responsive -->
-  <div class="container">
+  <div class="container pb-5">
     <div class="row">
       <div
         class="col-lg-4 col-md-5 col-11 col-sm-9 mb-4 d-flex justify-content-center px-3 mx-4 mx-md-0"
